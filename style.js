f9338de5b0f77b522b5a9c468e7a9a175609cc6c
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-03-05 23:01:04 +0300

    add provided an alert for invalid year and used function for validate

--- style.js
+++ style.js
@@ -2,3 +2,12 @@ var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+
+function validate() {
+    var genders = document.getElementsByName("gender");
+    if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
+       alert( "Please provide a valid year of birth! eg 2007" );
+       document.myForm.year.focus() ;
+       return false;
+    }
